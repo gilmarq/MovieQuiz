eb59fc15e28a85b87b7233d6d264ee9662d0c5b2
Gilmar Queiroz <gilmardequeiroz@hotmail.com>
2021-04-27 16:19:53 -0300

    criação de projeto

---
+++ MovieQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by Gilmar Queiroz on 27/04/21.
+//  Copyright © 2021 Gilmar Queiroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
