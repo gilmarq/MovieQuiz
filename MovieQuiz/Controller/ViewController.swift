d0ddf9dc03f5b04504a83ac512a5b2cce60ebc12
Gilmar Queiroz <gilmardequeiroz@hotmail.com>
2021-04-29 21:39:02 -0300

    criação da função de audio

--- MovieQuiz/Controller/ViewController.swift
+++ MovieQuiz/Controller/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -17,10 +18,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var ivQuiz: UIImageView!
     @IBOutlet weak var ivTime: UIView!
 
-
-    
     //MARK: - variable
     var quizManagr: QuizManager!
+    var quizPlayer: AVAudioPlayer!
 
     //MARK: - life cyclo
     override func viewDidLoad() {
@@ -32,9 +32,10 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         quizManagr = QuizManager()
         getNewQuiz()
+        starTime()
     }
 
-   //MARK: - action
+    //MARK: - action
     @IBAction func showHideSoundBar(_ sender: Any) {
         ivSoundBar.isHidden = !ivSoundBar.isHidden
     }
@@ -55,8 +56,47 @@ class ViewController: UIViewController {
         let round =  quizManagr.genereteRandonQuiz()
         for i in 0..<round.options.count {
             btOptions[i].setTitle(round.options[i].name, for: .normal)
+        }
+        playQuiz()
+    }
 
+    func playQuiz() {
+        guard let round = quizManagr.round else { return }
+        ivQuiz.image = UIImage(named: "movieSound")
+        if let url = Bundle.main.url(forResource:"quote\(round.quiz.number)", withExtension: "mp3") {
+            do {
+       quizPlayer = try  AVAudioPlayer(contentsOf:url)
+                quizPlayer.volume = 1
+                quizPlayer.delegate = self
+                quizPlayer.play()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+
+    }
+
+    func  starTime() {
+        ivTime.frame = view.frame
+        UIView.animate(withDuration:60.0 , delay: 0.0, options: .curveLinear, animations: {
+            self.ivTime.frame.size.width = 0
+            self.ivTime.frame.origin.x = self.view.center.x
+        }) { (success) in
+            self.gameOver()
         }
     }
+
+    func gameOver() {
+        performSegue(withIdentifier: "gameOverSegue", sender: nil)
+        quizPlayer.stop()
+    }
 }
+
+extension ViewController: AVAudioPlayerDelegate {
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        ivQuiz.image = UIImage(named: "movieSoundPause")
+    }
+}
+
 
